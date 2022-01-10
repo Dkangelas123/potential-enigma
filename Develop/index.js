53f327183266d2eb9be1afa14e98374d29e14620
Luis Chavez <Dkangelas@gmail.com>
2022-01-09 19:01:18 -0600

    index

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const util = require('util');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of messages for user input
+const questions = [{
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+}, {
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+},{
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+},{
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+},{
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+},{
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+},{
+    type: "input",
+    message: "dot dot dot",
+    name: "Title"
+},
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, function(error) {
+        console.log(fileName)
+        console.log(data)
+        if (error) {
+            return 
+            console.log(error)
+        } else {
+            console.log("Perfect")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions).then(function(data) {
+        writeToFile("ReadMe.md", generateMarkdown(data));
+        console.log(data)
+    })
+}
 
 // Function call to initialize app
 init();
